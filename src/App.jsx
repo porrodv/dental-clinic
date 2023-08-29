bfe0a64c7afda06ab23ea3aa5750ed88c1d98cde
Alexander Porro <alex1020pl@gmail.com>
2023-08-29 11:31:12 -0500

    init proyect

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App () {
+  return (
+    <>
+      <h1 style={{ height: ' 100vh', width: '100vw', display: 'grid', placeContent: 'center' }}>INICIO DEL PROYECTO</h1>
+    </>
+  );
+}
+
+export default App;
