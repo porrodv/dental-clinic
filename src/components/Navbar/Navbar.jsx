065f528cba9196965b0c4386aeed649de9d595f5
Alexander Porro <alex1020pl@gmail.com>
2023-09-20 11:42:18 -0500

    add navbar component & serve devDep

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,62 @@
+import { useState, useEffect } from 'react';
 import './styles.css';
 
 export default function Navbar () {
+  const [isSticky, setIsSticky] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  useEffect(() => {
+    // añade el evento al renderizar el componente
+    window.addEventListener('scroll', handleScroll);
+
+    // elimina el evento cuando cuando se deja de renderizar
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    window.scrollY > 0 ? setIsSticky(true) : setIsSticky(false);
+  };
+
+  const handleClick = () => {
+    setIsCollapsed(prevIsCollapsed => !prevIsCollapsed);
+  };
+
   return (
     <>
-      <nav className='navbar'>
-        <ul className='navbar__links'>
-          <li className='navbar__link'>
-            <a href='index.jsp'>Inicio</a>
-          </li>
-          <li className='navbar__link'>
-            <a href='#'>Nosotros</a>
-          </li>
-          <li className='navbar__link'>
-            <a href='#'>Servicios</a>
-          </li>
-          <li className='navbar__link'>
-            <a href='#'>Contacto</a>
-          </li>
-          <li id='contact-btn'>
-            <button><a href='#'>Reservar</a>
-            </button>
-          </li>
-        </ul>
+      {/* renderizado condicional de la clase */}
+      <nav className={`navbar ${isSticky ? 'sticky' : ''} ${isCollapsed ? 'collapsed' : ''}`}>
+        <div className='left'>
+          <div className='navbar__logo'>
+            <img src='../../public/dental_logo.png' alt='dental logo' />
+          </div>
+        </div>
+        <div className='right'>
+          <ul className='navbar__links'>
+            <li className='navbar__link'>
+              <a href='#'>Inicio</a>
+            </li>
+            <li className='navbar__link'>
+              <a href='#'>Nosotros</a>
+            </li>
+            <li className='navbar__link'>
+              <a href='#'>Servicios</a>
+            </li>
+            <li className='navbar__link'>
+              <a href='#'>Contacto</a>
+            </li>
+            <li>
+              <a href='#' id='navbar__reserve-btn' type='button'>
+                Reservar
+              </a>
+            </li>
+          </ul>
+        </div>
 
         <button
-          className='navbar__toggler'
+          id='navbar__toggler'
           typeof='button'
           title='navbar__toggler-btn'
+          onClick={handleClick}
         >
           <div className='line1' />
           <div className='line2' />
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,6 +4,40 @@ export default function MainPage () {
   return (
     <>
       <Navbar />
+
+      {/* ignora  */}
+      {/* ignora  */}
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum provident aperiam fugit consectetur fuga quidem sit ut asperiores rem deleniti impedit iusto accusantium facere vero corrupti, quaerat incidunt esse. Delectus.
     </>
   );
 }
